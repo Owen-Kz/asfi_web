bd30aa75ebc1dcbd506b52c11f57a267235ad49e
Benson Michael Owen <lordkz19@gmail.com>
2025-09-03 11:48:39 +0100

    removed

--- includes/db_connect.php
+++
@@ -1,4 +0,0 @@
-<?php 
- // $db = new mysqli('localhost', 'root', '', 'asfidb') or die('error in connection ');
-  $db = new mysqli('localhost', 'afriwqfk_asfidb', 'afriwqfk_asfidb', 'afriwqfk_asfidb') or die('error in connection ');
-?>
