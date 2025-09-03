c85a3f6071e6c27ff7c62c16886d05a6ebdf8b6b
Benson Michael Owen <lordkz19@gmail.com>
2025-09-03 20:26:33 +0100

    tempraril disabled https redirect

--- admin/header.php
+++ admin/header.php
@@ -6,11 +6,11 @@
 	 }
 ?>
 <?php
-if (! isset($_SERVER['HTTPS']) or $_SERVER['HTTPS'] == 'off' ) {
-    $redirect_url = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-    header("Location: $redirect_url");
-    exit();
-}
+// if (! isset($_SERVER['HTTPS']) or $_SERVER['HTTPS'] == 'off' ) {
+//     $redirect_url = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+//     header("Location: $redirect_url");
+//     exit();
+// }
 ?>
 <?php
 include ('../includes/db_connect.php');
