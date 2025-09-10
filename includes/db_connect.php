2faf0134a606403d9bad2145c745ab61385e60af
Benson Michael Owen <lordkz19@gmail.com>
2025-09-10 23:44:24 +0100

    Stop tracking sensitive files

--- includes/db_connect.php
+++
@@ -1,5 +0,0 @@
-<?php 
-  // $db = new mysqli('localhost', 'afriwqfk_asfidb', 'afriwqfk_asfidb', 'afriwqfk_asfidb') or die('error in connection ');
-  $db = new mysqli('localhost', 'root', '', 'afriwqfk_asfidb') or die('error in connection ');
-
-?>
